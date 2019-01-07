bece51fa82e2bfd421f041a3512920bda4ee0d43
anarmawala <arshadn00@gmail.com>
2019-01-06 19:48:08 -0600

    UI Dev

--- src/components/SignUpPage/index.js
+++ src/components/SignUpPage/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 
 import * as ROUTES from '../../constants/routes';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 const SignUpPage = () => (
   <div>
@@ -11,6 +17,7 @@ const SignUpPage = () => (
 );
 
 const BasicState = {
+  name: '',
   email: '',
   password: '',
   cp_password: '',
@@ -24,20 +31,70 @@ class SignUpForm extends React.Component {
     this.state = { ...BasicState };
   }
 
-  onSubmit = event => {};
+  handleSubmit = event => {};
 
-  onChange = event => {
+  handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
   };
 
   render() {
-    const { email, password, cp_password, error } = this.state;
+    const { email, password, cp_password, error, name } = this.state;
+    const { classes } = this.props;
 
+    const isInvalid =
+      password !== cp_password ||
+      password === '' ||
+      email === '' ||
+      name === '';
+
+    console.log(classes);
     return (
-      <form onSubmit={this.onSubmit}>
-        <div>Hello</div>
+      <form onSubmit={this.handleSubmit} noValidate autoComplete="off">
+        <TextField
+          id="outlined-name"
+          label="Full Name"
+          value={this.state.name}
+          name="name"
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-name"
+          label="Email"
+          value={this.state.email}
+          name="email"
+          type="email"
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-name"
+          label="Password"
+          value={this.state.password}
+          name="password"
+          type="password"
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-name"
+          label="Confirm Password"
+          value={this.state.cp_password}
+          name="cp_password"
+          type="password"
+          onChange={this.handleChange}
+          margin="normal"
+          variant="outlined"
+        />
+        <Button variant="contained" disabled={isInvalid} color="default">
+          Submit
+          {/* <CloudUploadIcon className={classes.rightIcon} /> */}
+        </Button>
       </form>
     );
   }
@@ -49,5 +106,10 @@ const SignUpLink = () => (
   </p>
 );
 
+SignUpForm.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
 export default SignUpPage;
+
 export { SignUpForm, SignUpLink };
